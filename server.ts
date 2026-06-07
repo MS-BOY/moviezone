a0cf789d90853f3c3859f9ff13b81b914ea7e52c
sany  amin <exsanyamin22@gmail.com>
2026-06-07 10:08:09 +0600

    Update server.ts

--- server.ts
+++ server.ts
@@ -116,39 +116,48 @@ app.get("/api/health", (req, res) => {
 
 // GET SITEMAP XML
 app.get("/sitemap.xml", async (req, res) => {
-  const host = "https://moviezone-9ogo.onrender.com";
-  const movies = await readMoviesAsync();
+  try {
+    const host = "https://moviezone-9ogo.onrender.com";
+    const movies = await readMoviesAsync();
 
-  let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
-  xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+    let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
+    xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
 
-  // Home Page
-  xml += `  <url>\n`;
-  xml += `    <loc>${host}/</loc>\n`;
-  xml += `    <changefreq>daily</changefreq>\n`;
-  xml += `    <priority>1.0</priority>\n`;
-  xml += `  </url>\n`;
+    // Home Page
+    xml += `
+  <url>
+    <loc>${host}/</loc>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>`;
 
-  // Admin Page
-  xml += `  <url>\n`;
-  xml += `    <loc>${host}/admin</loc>\n`;
-  xml += `    <changefreq>weekly</changefreq>\n`;
-  xml += `    <priority>0.5</priority>\n`;
-  xml += `  </url>\n`;
+    // Admin Page
+    xml += `
+  <url>
+    <loc>${host}/admin</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.5</priority>
+  </url>`;
 
-  // Movie posts sitemap entries
-  movies.forEach((m: any) => {
-    xml += `  <url>\n`;
-    xml += `    <loc>${host}/movie/${m.slug}</loc>\n`;
-    xml += `    <changefreq>weekly</changefreq>\n`;
-    xml += `    <priority>0.8</priority>\n`;
-    xml += `  </url>\n`;
-  });
+    // Movie Posts
+    movies.forEach((movie) => {
+      xml += `
+  <url>
+    <loc>${host}/movie/${movie.slug}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`;
+    });
 
-  xml += `</urlset>`;
+    xml += `\n</urlset>`;
 
-  res.setHeader("Content-Type", "application/xml");
-  res.send(xml);
+    res.header("Content-Type", "application/xml");
+    res.send(xml);
+
+  } catch (error) {
+    console.error("Sitemap Error:", error);
+    res.status(500).send("Error generating sitemap");
+  }
 });
 
 // GET movies list (supports search, genres, year filter, sorting)
